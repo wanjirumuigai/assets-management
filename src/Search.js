68b7548fd2551efb50b0ca8d16a5795e7abbf3d1
jesse <jesse.kibaara@student.moringaschool.com>
2023-02-21 21:58:01 +0300

    asset search props mispelling corrected

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,19 @@
 import React, { useState } from 'react';
+
 function AssetSearch(props) {
-    const [searchTerm, setSearchTerm] = useState('');
-    function handleSearch(event) {
-        event.preventDefault();
-        props.onSearch(searchTerm);  
+  const [searchItem, setSearchItem] = useState('');
+
+  function handleSearch(event) {
+    event.preventDefault();
+    props.onSearch(searchItem);
+  }
+
+  return (
+    <form onSubmit={handleSearch}>
+      <input type="text" value={searchItem} onChange={(event) => setSearchItem(event.target.value)} placeholder="Search by name or serial number" />
+      <button type="submit">Search</button>
+    </form>
+  );
+}
+
+export default AssetSearch;

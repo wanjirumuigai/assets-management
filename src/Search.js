b1c2e442ffb07574cece910ddb13cead62cc82a4
jesse <jesse.kibaara@student.moringaschool.com>
2023-02-21 21:12:27 +0300

    event handler created

--- src/Search.js
+++ src/Search.js
@@ -1,3 +1,6 @@
 import React, { useState } from 'react';
 function AssetSearch(props) {
     const [searchTerm, setSearchTerm] = useState('');
+    function handleSearch(event) {
+        event.preventDefault();
+        props.onSearch(searchTerm);  

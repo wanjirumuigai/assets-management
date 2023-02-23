139cc0aef8ea1b704c20461b96f08a50661f81be
jesse <jesse.kibaara@student.moringaschool.com>
2023-02-23 17:37:21 +0300

    test added

--- src/Search.js
+++ src/Search.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 
 function AssetSearch(props) {
-  const [searchItem, setSearchItem] = useState('');
+  const [searchItem, setSearchTerm] = useState('');
 
   function handleSearch(event) {
     event.preventDefault();
-    props.onSearch(searchItem);
+    props.onSearch(searchTerm);
   }
 
   return (
     <form onSubmit={handleSearch}>
-      <input type="text" value={searchItem} onChange={(event) => setSearchItem(event.target.value)} placeholder="Search by name or serial number" />
+      <input type="text" value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)} placeholder="Search by name or serial number" />
       <button type="submit">Search</button>
     </form>
   );
